6f96ebb4ec7ed92c8cfccef7ca5103d7b4db792b
Chris Boesch <chrisboesch@nus.edu.sg>
2017-04-14 15:42:29 +0800

    Getting user code from ipynb in event and running multiple tests

--- python/usercode.py
+++
@@ -1,6 +0,0 @@
-def subtract(a,b):
-    return a-b
-
-def sum(a,b):
-    #raise ValueError('A very specific bad thing happened')
-    return a+b
--- python/verifier.py
+++ python/verifier.py
@@ -1,6 +1,6 @@
 #docker run -v "$PWD":/var/task lambci/lambda:python2.7 python/lambda_function.lambda_handler
 
-# run passing, failing, and erroring tests. 
+# run passing, failing, and erroring tests.
 # docker run -v "$PWD":/var/task lambci/lambda:python2.7 python/verifier.lambda_handler '{"tests": {"1":"self.assertEqual(2,2)","2": "self.assertEqual(sum(1,2),3)","3": "self.assertEqual(1,2)","4": "self.assertEqual()"}}'
 
 # Just a test lambda, run with:
@@ -14,49 +14,44 @@ import json
 # import * is only allowed from the root module level. 
 #from usercode import *
 
-# TODO: update handler to call run tests using test from passed in event data
+def save_usercode(ipynbJSON):
+    j = ipynbJSON
+    of = open("usercode.py", 'w') #usercode.py
+    if j["nbformat"] >=4:
+        for i,cell in enumerate(j["cells"]):
+                of.write("#cell "+str(i)+"\n")
+                for line in cell["source"]:
+                        of.write(line)
+                of.write('\n\n')
+    else:
+        for i,cell in enumerate(j["worksheets"][0]["cells"]):
+                of.write("#cell "+str(i)+"\n")
+                for line in cell["input"]:
+                        of.write(line)
+                of.write('\n\n')
+    of.close()
+
 
 def lambda_handler(event, context):
-    #print(event)
-    
-    #print(theResults)
-    
-    #TODO save the usercode from event data.     
-    
+    save_usercode(event)
     import test_runner
-    #some_tests = {"1":"self.assertEqual(2,2)",
-    #    "2": "self.assertEqual(sum(1,2),3)", #usercode
-    #    "3": "self.assertEqual(1,2)", #failing assert
-    #    "4": "self.assertEqual()" # Bad assert
-    #}
-    #theResults = run_tests(some_tests)
-    
     theResults = test_runner.run_tests(event["tests"])
-    
-    #Test to see if any non-serializable python types are in the result. 
-    #jsonResults = json.dumps(theResults)
-    #print("---- Printing from container ------\n")
-    
+    try:
+        os.remove("usercode.py")
+        os.remove("usercode.pyc")
+    except OSError:
+        pass
+
     from pprint import pprint
     pprint(theResults)
     print("\n---- End printing from container ------")
-
-    #data = {"some":"data"}
-    #print(type(context))
-    #print(dir(context))
-    #print(context.client_context)
-    #print(context.xray_context)
-    
-
-    #context.log("hello")
     return theResults
 
 if __name__ == "__main__":
     print("Running from command line")
     passed_in_tests = {"5":"self.assertEqual(2,2)",
-        "6": "self.assertEqual(sum(1,2),3)", #usercode
-        "7": "self.assertEqual(1,2)", #failing assert
-        "8": "self.assertEqual()" # Bad assert
+        "6": "self.assertEqual(1,2)", #failing assert
+        "7": "self.assertEqual()" # Bad assert
     }
 
     theResults = run_tests(passed_in_tests)
